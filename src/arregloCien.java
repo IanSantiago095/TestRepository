a5845907db27c6d3cb354593e3f8dc218310c5d1
halo_ <halo_gee@hotmail.com>
2024-12-03 23:56:33 -0700

    Arreglo 100 elementos

---
+++ src/arregloCien.java
@@ -0,0 +1,13 @@
+public class arregloCien {
+    public static void main(String[] args) {
+        int[] arreglo = new int[100];
+
+        for (int i = 0; i < 100; i++) {
+            arreglo[i] = 1000 + i;
+        }
+
+        for (int i = 99; i >= 0; i--) {
+            System.out.println(arreglo[i]);
+        }
+    }
+}
